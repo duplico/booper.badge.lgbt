009b98bfe04aa1c216bed731a2c7838da9eef07e
George Louthan <duplico@dupli.co>
2023-07-14 17:16:27 -0500

    Add badge.h

---
+++ ccs_workspace/rogue_badge_2023/badge.h
@@ -0,0 +1,20 @@
+/// mini.badge.lgbt 2023 main header file.
+/**
+ ** This header contains the core configuration and functionality declarations
+ ** and definitions for the Queercon 2022 badge.
+ **
+ ** \file badge.h
+ ** \author George Louthan
+ ** \date   2022
+ ** \copyright (c) 2022 George Louthan @duplico. MIT License.
+ */
+
+#ifndef BADGE_H_
+#define BADGE_H_
+
+/// MCLK rate in MHZ.
+#define MCLK_FREQ_MHZ 8
+/// SMCLK rate in Hz.
+#define SMCLK_RATE_HZ 8000000
+
+#endif /* BADGE_H_ */
