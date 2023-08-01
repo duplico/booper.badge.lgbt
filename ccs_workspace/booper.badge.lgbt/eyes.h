f73c63971338ee7d6befe820928b3221fa548dba
George Louthan <duplico@dupli.co>
2023-08-01 18:42:18 -0500

    Add additional ambient eye shapes.

--- ccs_workspace/booper.badge.lgbt/eyes.h
+++ ccs_workspace/booper.badge.lgbt/eyes.h
@@ -11,14 +11,34 @@
 typedef enum {
     EYES_NORMAL = 0,
     EYES_HAPPY,
+    EYES_SAD,
+    EYES_BIG,
+    EYES_LEFT,
+    EYES_RIGHT,
+    EYES_BIGHAPPY,
+    EYES_DAFUQ,
     EYES_COUNT
 } eyes_disp_t;
 
+/*       4 (dot)
+ *  0 5
+ * 1 3 6
+ *  2 7
+ */
+
 #define CIRCLE_LEFT (eye_t) {1, 1, 1, 1, 0, 0, 0, 0}
 #define CIRCLE_RIGHT (eye_t) {0, 0, 0, 1, 0, 1, 1, 1}
+#define CIRCLE_BIG (eye_t) {1,1,1,0,0,1,1,1}
 
 #define HAPPY_LEFT (eye_t) {1, 1, 0, 1, 0, 0, 0, 0}
 #define HAPPY_RIGHT (eye_t) {0, 0, 0, 1, 0, 1, 1, 0}
+#define HAPPY_BIG (eye_t) {1,1,0,0,0,1,1,0}
+
+#define SAD_LEFT (eye_t) {0,1,1,1,0,0,0,0}
+#define SAD_RIGHT (eye_t) {0,0,0,1,0,0,1,1}
+
+#define SIDE_LEFT (eye_t) {1,1,1,0,0,0,0}
+#define SIDE_RIGHT (eye_t) {0,0,0,0,0,1,1,1}
 
 #define BLINK_RIGHT (eye_t) {0,0,0,0,0,0,0,1}
 #define BLINK_LEFT (eye_t)  {0,0,1,0,0,0,0,0}
