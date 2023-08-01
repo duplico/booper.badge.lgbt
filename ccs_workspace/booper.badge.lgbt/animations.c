e4b7deccd493159215d7665692b6df4f0f21dadf
George Louthan <duplico@dupli.co>
2023-08-01 18:23:23 -0500

    Rework stored blinking animations.

--- ccs_workspace/booper.badge.lgbt/animations.c
+++ ccs_workspace/booper.badge.lgbt/animations.c
@@ -8,23 +8,8 @@
 #include "leds.h"
 #include "eyes.h"
 
-eye_anim_frame_t anim_blink[] = {(eye_anim_frame_t ){
-                            {
-                             (eye_t) {0,0,0,0,0,0,0,1},
-                             (eye_t) {0,0,1,0,0,0,0,0},
-                            },
-                            30,
-                            1
-}};
-
 eye_anim_frame_t anim_shifty[] = {
     (eye_anim_frame_t) {
-        (eye_t) {0,0,0,0,0,0,0,1},
-        (eye_t) {0,0,1,0,0,0,0,0},
-        20,
-        0
-    },
-    (eye_anim_frame_t) {
         {CIRCLE_LEFT, CIRCLE_LEFT},
         20,
         0
@@ -61,12 +46,6 @@ eye_anim_frame_t anim_shifty[] = {
     },
     (eye_anim_frame_t) {
         {CIRCLE_RIGHT, CIRCLE_RIGHT},
-        20,
-        0
-    },
-    (eye_anim_frame_t) {
-        (eye_t) {0,0,0,0,0,0,0,1},
-        (eye_t) {0,0,1,0,0,0,0,0},
         20,
         1
     },
