f095271cd3d296b25f3bbcba0cf99b3b9ff6a2e5
George Louthan <duplico@dupli.co>
2023-08-01 21:52:50 -0500

    Add some more animations

--- ccs_workspace/booper.badge.lgbt/animations.c
+++ ccs_workspace/booper.badge.lgbt/animations.c
@@ -25,6 +25,88 @@ eye_anim_t anim_shifty = (eye_anim_t) {
     .loop_count = 3,
 };
 
+eye_anim_frame_t anim_dafuq_frames[] = {
+    (eye_anim_frame_t) {
+        {CIRCLE_RIGHT, CIRCLE_BIG},
+        50,
+    },
+    (eye_anim_frame_t) {
+        {CIRCLE_BIG, CIRCLE_LEFT},
+        50,
+    },
+};
+
+eye_anim_t anim_dafuq = (eye_anim_t) {
+    .frames = anim_dafuq_frames,
+    .length = 2,
+    .loop_count = 4,
+};
+
+eye_anim_frame_t anim_happytoggle_frames[] = {
+    (eye_anim_frame_t) {
+        {HAPPY_LEFT, HAPPY_RIGHT},
+        75,
+    },
+    (eye_anim_frame_t) {
+        {HAPPY_RIGHT, HAPPY_LEFT},
+        75,
+    },
+};
+
+eye_anim_t anim_happytoggle = (eye_anim_t) {
+    .frames = anim_happytoggle_frames,
+    .length = 2,
+    .loop_count = 6,
+};
+
+eye_anim_frame_t anim_happywink_frames[] = {
+    (eye_anim_frame_t) {
+        {HAPPY_BIG, BLINK_WIDE},
+        150,
+    },
+};
+
+eye_anim_t anim_happywink = (eye_anim_t) {
+    .frames = anim_happywink_frames,
+    .length = 1,
+    .loop_count = 0,
+};
+
+eye_anim_frame_t anim_wink_frames[] = {
+    (eye_anim_frame_t) {
+        {CIRCLE_RIGHT, BLINK_LEFT},
+        80,
+    },
+};
+
+eye_anim_t anim_wink = (eye_anim_t) {
+    .frames = anim_wink_frames,
+    .length = 1,
+    .loop_count = 0,
+};
+
+eye_anim_frame_t anim_dirshifty_frames[] = {
+    (eye_anim_frame_t) {
+        {SIDE_LEFT, SIDE_LEFT},
+        45,
+    },
+    (eye_anim_frame_t) {
+        {SIDE_RIGHT, SIDE_RIGHT},
+        45,
+    }
+};
+
+eye_anim_t anim_dirshifty = (eye_anim_t) {
+    .frames = anim_dirshifty_frames,
+    .length = 2,
+    .loop_count = 3,
+};
+
 eye_anim_t *animations[] = {
     &anim_shifty,
+    &anim_dafuq,
+    &anim_happytoggle,
+    &anim_happywink,
+    &anim_wink,
+    &anim_dirshifty,
 };
